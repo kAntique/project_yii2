0a97848dcba1fa0b4959859a087d927db20d7a86
kritsada <kritsada.swe@gmail.com>
2016-08-02 14:23:05 +0700

    generate module customer

--- yii2_test/backend/config/main.php
+++ yii2_test/backend/config/main.php
@@ -15,6 +15,9 @@ return [
       'company' => [
             'class' => 'backend\modules\company\Module',
         ],
+        'customer' => [
+            'class' => 'backend\modules\customer\Module',
+        ],
     ],
     'components' => [
         'request' => [
