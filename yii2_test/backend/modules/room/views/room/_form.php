30fbc267f6ff32b089c55b43b8926dabb1ada6d8
kritsada <kritsada.swe@gmail.com>
2016-08-02 16:18:17 +0700

    add widget and modified form

--- yii2_test/backend/modules/room/views/room/_form.php
+++ yii2_test/backend/modules/room/views/room/_form.php
@@ -1,8 +1,10 @@
 <?php
-
+use dosamigos\selectize\SelectizeTextInput;
+use dosamigos\fileupload\FileUploadUI;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-
+use kartik\color\ColorInput;
+use dosamigos\selectize\SelectizeDropDownList;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\room\models\Room */
 /* @var $form yii\widgets\ActiveForm */
@@ -13,15 +15,26 @@ use yii\widgets\ActiveForm;
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'name')->textInput() ?>
+<?php echo '<b>ประเภทห้อง : </b>';?>
+    <?php echo SelectizeTextInput::widget([ 'name' => 'type',
+    'value' => 'sweet', 'clientOptions' => [  [] ], ]);
+    echo SelectizeTextInput::widget([ 'name' => 'type',
+    'value' => 'single', 'clientOptions' => [  [] ], ]);
+    echo SelectizeTextInput::widget([ 'name' => 'type',
+    'value' => 'vip', 'clientOptions' => [  [] ], ]); ?>
 
-    <?= $form->field($model, 'type')->textInput() ?>
-
-    <?= $form->field($model, 'color')->textInput() ?>
+    <?php echo $form->field($model, 'color')->widget(ColorInput::classname(), [
+    'options' => ['placeholder' => 'Select color ...'] ]);?>
 
     <?= $form->field($model, 'detail')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'picture')->textInput(['maxlength' => true]) ?>
-
+    <?php echo '<b>รูปห้อง : </b>';?>
+    <?= FileUploadUI::widget([ 'model' => $model, 'attribute' => 'picture',
+    'url' => ['media/upload', 'id' => $model], 'gallery' => false,
+     'fieldOptions' => [ 'accept' => 'image/*' ], 'clientOptions' => [ 'maxFileSize' => 2000000 ],
+      'clientEvents' => [ 'fileuploaddone' => 'function(e, data) { console.log(e); console.log(data); }',
+       'fileuploadfail' => 'function(e, data) { console.log(e); console.log(data); }', ], ]);
+      ?>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
